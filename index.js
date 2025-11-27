1e23f004a0598cadac3c9d2d180c9df603e357da
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-11-27 19:43:42 +0530

    up

--- index.js
+++ index.js
@@ -1,73 +1,16 @@
 
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import connectDB from "./utils/db.js";
-// import userRoute from "./routes/user.route.js";
-// import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-
-// const corsOptions = {
-//   origin: "https://jobportalservice.netlify.app",  
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-
-
-// app.use(cors(corsOptions));
-
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://jobportalservice.netlify.app");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-
-//   next();
-// });
-
-// const PORT = process.env.PORT || 5001;
-
-// // Routes
-// app.use("/api/user", userRoute);
-// app.use("/api/company", companyRoute);
-// app.use("/api/job", jobRoute);
-// app.use("/api/application", applicationRoute);
-
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`✅ Server running Running Successfully `);
-// });
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
 dotenv.config();
+
 const app = express();
 
 // Middlewares
@@ -75,19 +18,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// ✅ Proper CORS for Netlify + Cookies
+
 const corsOptions = {
-  origin: [
-    "https://jobportalservice.netlify.app",
-    "http://localhost:5173"
-  ],
+  origin: "https://jobportalservice.netlify.app",  
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://jobportalservice.netlify.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+const PORT = process.env.PORT || 5001;
 
 // Routes
 app.use("/api/user", userRoute);
@@ -95,14 +51,7 @@ app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("✅ Job Portal API Running");
-});
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, async () => {
-  await connectDB();
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`✅ Server running Running Successfully `);
 });
